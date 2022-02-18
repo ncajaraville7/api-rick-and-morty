fe0ee56230d38d109027692c1074529d74934750
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-02-18 15:32:53 -0300

    remove scroll infinite

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Stack, Spinner } from '@chakra-ui/react'
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { Stack } from '@chakra-ui/react'
 
 import Character from './Character'
 import CharacterSearch from './CharacterSearch';
@@ -15,22 +14,20 @@ const CharacterList = ({ favorite, setFavorite }) => {
     
     fetch(CHARACTER_API)
       .then( res => res.json())
-      .then( data => setCharacters((prevCharacter) => prevCharacter.concat(data.results)))
+      .then( data => setCharacters(data.results))
   }
 
   useEffect( () => {
     getCharacter();
-  }, [page])
+  }, [])
 
   return (
     <>
       <CharacterSearch getCharacter={getCharacter} setCharacters={setCharacters} />
   
-      <InfiniteScroll dataLength={characters.length} hasMore={true} next={ () => setPage( (prevPage) => prevPage + 1)}>
-        <Stack direction='row' wrap='wrap' justify='center' aling='center' gap={8} p={8}>
-          { characters.map( item => <Character key={item.id} {...item} favorite={favorite} setFavorite={setFavorite} />) }
-        </Stack>
-      </InfiniteScroll>
+      <Stack direction='row' wrap='wrap' justify='center' aling='center' gap={8} p={8}>
+        { characters.map( item => <Character key={item.id} {...item} favorite={favorite} setFavorite={setFavorite} />) }
+      </Stack>
     </>
   )
 }
