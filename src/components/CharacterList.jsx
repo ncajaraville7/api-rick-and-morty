e970ea715ea8c24adea2dd1484af5e9c9cf639cd
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-02-18 15:23:13 -0300

    add scroll infinite

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Stack } from '@chakra-ui/react'
+import { Stack, Spinner } from '@chakra-ui/react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import Character from './Character'
 import CharacterSearch from './CharacterSearch';
@@ -7,26 +8,29 @@ import CharacterSearch from './CharacterSearch';
 const CharacterList = ({ favorite, setFavorite }) => {
 
   const [ characters, setCharacters ] = useState([]);
+  const [ page, setPage ] = useState(1);
 
   const getCharacter = () => {
-    const CHARACTER_API = 'https://rickandmortyapi.com/api/character';
-
+    const CHARACTER_API = 'https://rickandmortyapi.com/api/character/?page=' + page;
+    
     fetch(CHARACTER_API)
       .then( res => res.json())
-      .then( data => setCharacters(data.results))
+      .then( data => setCharacters((prevCharacter) => prevCharacter.concat(data.results)))
   }
 
   useEffect( () => {
     getCharacter();
-  }, [])
+  }, [page])
 
   return (
     <>
       <CharacterSearch getCharacter={getCharacter} setCharacters={setCharacters} />
-
-      <Stack direction='row' wrap='wrap' justify='center' aling='center' gap={8} p={8}>
-        { characters.map( item => <Character key={item.id} {...item} favorite={favorite} setFavorite={setFavorite} />) }
-      </Stack>
+  
+      <InfiniteScroll dataLength={characters.length} hasMore={true} next={ () => setPage( (prevPage) => prevPage + 1)}>
+        <Stack direction='row' wrap='wrap' justify='center' aling='center' gap={8} p={8}>
+          { characters.map( item => <Character key={item.id} {...item} favorite={favorite} setFavorite={setFavorite} />) }
+        </Stack>
+      </InfiniteScroll>
     </>
   )
 }
