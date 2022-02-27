f7fc71620737e0913ae8f0253a8b2801c5203308
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-02-27 15:50:07 -0300

    Fix responsive

--- src/components/CharacterFavorites.jsx
+++ src/components/CharacterFavorites.jsx
@@ -15,7 +15,7 @@ const CharacterFavorites = ({ favorite, setFavorite }) => {
       favorite.map( item => (
         <Stack direction='column' aling='center' p={6} key={item.characterId}>
           <Container maxW='80%'>
-            <Stack direction='row' justify='space-between' align='center' backgroundColor='gray.700' p={4}>
+            <Stack direction={{base: 'column', md: 'row'}} justify='space-between' align='center' backgroundColor='gray.700' p={4}>
               <Image src={item.characterImg} alt={item.characterName} h='155px' />
               <Heading as='h4'>{item.characterName}</Heading>
               <Button backgroundColor='red.600' _hover={{backgroundColor: 'red.500'}} onClick={ () => deleteCharacter(item.characterId)}>Eliminar</Button>
